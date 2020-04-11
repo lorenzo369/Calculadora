25c48a3b855112bd6b909be565e63619da5e6a33
Lorenzo Sanchez <lorenzo369@hotmail.com>
2020-04-11 15:58:30 +0200

    cambio en la jerarquia

---
+++ src/main/java/com/rest/resta/RestaServer.java
@@ -0,0 +1,21 @@
+package com.rest.resta;
+
+import java.util.logging.Logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@ComponentScan
+public class RestaServer {
+	protected Logger logger = Logger.getLogger(RestaServer.class.getName());
+
+	public static void main(String[] args) {
+		System.setProperty("spring.config.name", "resta-server");
+
+		SpringApplication.run(RestaServer.class,args);
+	}
+}
---
+++ src/main/java/com/rest/suma/SumaServer.java
@@ -0,0 +1,22 @@
+package com.rest.suma;
+
+import java.util.logging.Logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@ComponentScan
+public class SumaServer {
+
+	protected Logger logger = Logger.getLogger(SumaServer.class.getName());
+
+	public static void main(String[] args) {
+		System.setProperty("spring.config.name", "suma-server");
+
+		SpringApplication.run(SumaServer.class,args);
+	}
+}
