3f50402e05b6c73ce56d7f5bf76cfa2d94fd6e73
Lorenzo Sanchez <lorenzo369@hotmail.com>
2020-04-17 16:32:28 +0200

    Implementacion de sumar

---
+++ src/main/java/impl/OperacionesImpl.java
@@ -0,0 +1,24 @@
+package impl;
+
+import interfaces.Operaciones;
+
+public class OperacionesImpl implements Operaciones {
+
+	@Override
+	public String sumar(String operando1, String operando2) {
+		int iOperando1 = Integer.valueOf(operando1);
+		int iOperando2 = Integer.valueOf(operando2);
+		int suma = iOperando1 + iOperando2;
+
+		return "{\"operando1\":\"" + operando1 + "\", \"operando2\":\"" + operando2 + "\", \"suma\": \"" + suma + "\"}";
+	}
+
+	@Override
+	public String restar(String operando1, String operando2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
