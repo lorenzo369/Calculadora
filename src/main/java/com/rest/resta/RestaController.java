67b09f8c1ab28202157e1fe85b696f4ec6b324e2
Lorenzo Sanchez <lorenzo369@hotmail.com>
2020-04-18 13:50:54 +0200

    Añadir un verbo a llamadas Rest

--- src/main/java/com/rest/resta/RestaController.java
+++ src/main/java/com/rest/resta/RestaController.java
@@ -2,23 +2,22 @@ package com.rest.resta;
 
 import java.util.logging.Logger;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import impl.OperacionesImpl;
 
 @RestController
 public class RestaController {
 	protected Logger logger = Logger.getLogger(RestaController.class
 			.getName());
 
-	@RequestMapping("/restar")
+	@GetMapping(value="/restar")
 	public String doRestar(@RequestParam(value="restador1") String sOperando1,
 			@RequestParam(value="restador2") String sOperando2) {
 
-		int iOperando1 = Integer.valueOf(sOperando1);
-		int iOperando2 = Integer.valueOf(sOperando2);
-		int resultado = iOperando1 - iOperando2;
-
-		return "{\"iOperando1\":\"" + sOperando1 + "\", \"iOperando2\":\"" + sOperando2 + "\", \"resultado\": \"" + resultado + "\"}";
+		OperacionesImpl operacionesImpl = new OperacionesImpl();
+		return operacionesImpl.restar(sOperando1, sOperando2);
 	}
 }
--- src/main/java/com/rest/suma/SumaControlador.java
+++ src/main/java/com/rest/suma/SumaControlador.java
@@ -2,9 +2,11 @@ package com.rest.suma;
 
 import java.util.logging.Logger;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import impl.OperacionesImpl;
 
 @RestController
 public class SumaControlador {
@@ -12,14 +14,12 @@ public class SumaControlador {
 	protected Logger logger = Logger.getLogger(SumaControlador.class
 			.getName());
 
-	@RequestMapping("/sumar")
+	@GetMapping("/sumar")
 	public String doSumar(@RequestParam(value="operando1") String operando1,
 			@RequestParam(value="operando2") String operando2) {
 
-		int iOperando1 = Integer.valueOf(operando1);
-		int iOperando2 = Integer.valueOf(operando2);
-		int suma = iOperando1 + iOperando2;
-
-		return "{\"operando1\":\"" + operando1 + "\", \"operando2\":\"" + operando2 + "\", \"suma\": \"" + suma + "\"}";
+		OperacionesImpl operacionesImpl = new OperacionesImpl();
+		return operacionesImpl.sumar(operando1, operando2);
+	
 	}
 }
