62c42f0329d4f11bd9a8b1df0e13090be8c20941
Lorenzo Sanchez <lorenzo369@hotmail.com>
2020-04-18 12:23:43 +0200

    Reemplazar valueOf por parseInt

--- src/main/java/impl/OperacionesImpl.java
+++ src/main/java/impl/OperacionesImpl.java
@@ -6,8 +6,8 @@ public class OperacionesImpl implements Operaciones {
 
 	@Override
 	public String sumar(String operando1, String operando2) {
-		int iOperando1 = Integer.valueOf(operando1);
-		int iOperando2 = Integer.valueOf(operando2);
+		int iOperando1 = Integer.parseInt(operando1);
+		int iOperando2 = Integer.parseInt(operando2);
 		int suma = iOperando1 + iOperando2;
 
 		return "{\"operando1\":\"" + operando1 + "\", \"operando2\":\"" + operando2 + "\", \"suma\": \"" + suma + "\"}";
@@ -15,8 +15,8 @@ public class OperacionesImpl implements Operaciones {
 
 	@Override
 	public String restar(String operando1, String operando2) {
-		int iOperando1 = Integer.valueOf(operando1);
-		int iOperando2 = Integer.valueOf(operando2);
+		int iOperando1 = Integer.parseInt(operando1);
+		int iOperando2 = Integer.parseInt(operando2);
 		int resultado = iOperando1 - iOperando2;
 
 		return "{\"iOperando1\":\"" + operando1 + "\", \"iOperando2\":\"" + operando2 + "\", \"resultado\": \"" + resultado + "\"}";
