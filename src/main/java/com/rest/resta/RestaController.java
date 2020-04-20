6cbc77b2d5d2752a72d072b111048b29ea57e19f
Lorenzo Sanchez <lorenzo369@hotmail.com>
2020-04-20 13:41:40 +0200

    eliminar instancias ajenas

--- src/main/java/com/rest/resta/RestaController.java
+++ src/main/java/com/rest/resta/RestaController.java
@@ -1,10 +1,10 @@
 package com.rest.resta;
 
+import static com.StaticFactoryBuilder.OPERACION;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import impl.OperacionesImpl;
 
 @RestController
 public class RestaController {
@@ -13,7 +13,6 @@ public class RestaController {
 	public String doRestar(@RequestParam(value="restador1") String sOperando1,
 			@RequestParam(value="restador2") String sOperando2) {
 
-		OperacionesImpl operacionesImpl = new OperacionesImpl();
-		return operacionesImpl.restar(sOperando1, sOperando2);
+		return OPERACION.restar(sOperando1, sOperando2);
 	}
 }
